eb1186358d9bc6216c111d23e213b60f789cab20
tofi1130 <taafuuu@gmail.com>
2023-07-29 19:48:13 +0500

    Introduce core function

--- functions.h
+++ functions.h
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void
+print_n_after (FILE *fp, int starting_line_number, int ending_line_number)
+{
+    int line_idx = 1;
+    char character;
+
+    while ((character = fgetc(fp)) != EOF)
+    {
+        if (character == '\n')
+            line_idx++;
+        
+        if (line_idx >= starting_line_number)
+        {
+            if (line_idx == ending_line_number)
+                break;
+            
+            putchar (character);
+        }
+    }
+}
