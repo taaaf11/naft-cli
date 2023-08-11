a22a72a4b16128c6cd74594fb301aec2e819094e
tofi1130 <taafuuu@gmail.com>
2023-08-11 17:37:40 +0500

    fixed some typos

--- naft.c
+++ naft.c
@@ -4,23 +4,24 @@
 #include <getopt.h>
 
 
-static flag 
-		s_switch = 0,
-		r_switch = 0,
-		a_switch = 0,
-		o_switch = 0,
-		f_switch = 0,
-		n_switch = 0,
-		ask_help = 0;
+static flag
+        aflag    = 0, // a switch is present.
+		s_switch = 0, // 1 if -s is present; specify starting line number when giving range.
+		r_switch = 0, // 1 if -r is present; specify ending line number when giving range.
+		a_switch = 0, // 1 if -a is present; print all lines after the line at given line number.
+		o_switch = 0, // 1 if -o is present; print one line at given line number.
+		f_switch = 0, // 1 if -f is present; get input from specified file instead of from stdin.
+		n_switch = 0, // 1 if -n is present; don't print trailing newline at end of output.
+		ask_help = 0; // 1 if -h is present; help message.
 	
 
 struct option longoptions[] = {
 	{"starting-line", required_argument, 0, 's'},
-	{"required-lines", required_argument, 0, 'r'},
+	{"ending-line", required_argument, 0, 'r'},
 	{"all-after", required_argument, 0, 'a'},
 	{"just-one", required_argument, 0, 'o'},
-	{"from-file", no_argument, 0, 'f'},
-	{"no-newline", no_argument, 0, 'n'},
+	{"from-file", required_argument, 0, 'f'},
+	{"no-trailn", no_argument, 0, 'n'},
 	{"help", no_argument, 0, 'h'},
 	{0, 0, 0, 0}
 };
@@ -43,45 +44,55 @@ main (int argc, char **argv)
 		{
 			case 's':
 			    make_true(s_switch);
+			    make_true(aflag);
 				range_starting_line = atoi(optarg);
-			    break;
+				break;
 			
 			case 'r':
 			    make_true(r_switch);
+			    make_true(aflag);
 				range_ending_line = atoi(optarg);
 				break;
 			
 			case 'a':
 			    make_true(a_switch);
+			    make_true(aflag);
 				line_start_a_o = atoi(optarg);
 			    break;
 			
 			case 'o':
 			    make_true(o_switch);
+			    make_true(aflag);
 				line_start_a_o = atoi(optarg);
 				break;
 			
 			case 'f':
 			    make_true(f_switch);
+			    make_true(aflag);
 				file_name = optarg;
 				break;
 			
 			case 'n':
 			    make_true(n_switch);
+			    make_true(aflag);
 				break;
 			
 			case 'h':
 			    make_true(ask_help);
+			    make_true(aflag);
 				break;
 		}
 	}
+
+	if (!aflag)
+	    return 0;
 
 	if (ask_help)
 	    print_usage(0); // function auto-exits.
 	
 	if ((s_switch && !r_switch) || (!s_switch && r_switch))
 	{
-	    fprintf(stderr, "-s and -r switches with their respective long options are to be used together.\n");
+	    fprintf(stderr, "-s and -r switches are to be used together.\n");
 		exit(EXIT_FAILURE);
 	}
 	
@@ -98,20 +109,20 @@ main (int argc, char **argv)
 		
 		if (!n_switch)
 		    trail_ln(1);
-		
+
 		return 0;
 	}
 
 	FILE *file = fopen(file_name, "r");
 
 	if (a_switch)
-		    print_all_after(stdin, line_start_a_o);
+		print_all_after(file, line_start_a_o);
 		
 	if (o_switch)
-	    print_one_line(stdin, line_start_a_o);
+	    print_one_line(file, line_start_a_o);
 		
 	if (s_switch && r_switch)
-	    print_range(stdin, range_starting_line, range_ending_line);
+	    print_range(file, range_starting_line, range_ending_line);
 		
 	if (!n_switch)
 	    trail_ln(1);
--- usage.h
+++ usage.h
@@ -7,26 +7,30 @@ print_usage (int status)
 {
     if (! (status == EXIT_SUCCESS) )
     {
-        fprintf(stderr, "USAGE: naft OPTION(S) FILE \n"
-                        " or    naft OPTION(S) -i\n\n");
+        fprintf(stderr, "USAGE: naft OPTION(S)\n"
+                        "  or   naft OPTION(S) -f FILE \n\n");
 
-        fprintf(stderr, "--starting-line,  -s <number>\t\tWhen specifying range (stating). Used with -r switch.\n"
-                        "--required-lines, -r <number>\t\tWhen specifying range (ending). Used with -s switch.\n"
-                        "--all-after,      -a <number>\t\tPrint all lines after given line.\n"
-                        "--just-one,       -o <number>\t\tPrint one line after the given line.\n"
-                        "--from-stdin,     -i\t\t\tGet input from stdin instead of from FILE.\n"
-                        "--help, -h\t\t\t\tPrint this help message.");
+        fprintf(stderr, "    --starting-line,  -s <number>\t\tSpecify range (stating). Used with -r switch.\n"
+                        "    --ending-line,    -r <number>\t\tSpecify range (ending). Used with -s switch.\n"
+                        "    --all-after,      -a <number>\t\tPrint all lines after given line.\n"
+                        "    --just-one,       -o <number>\t\tPrint one line after the given line.\n"
+                        "    --from-file,      -f\t\t\tGet input from file instead of from stdin.\n"
+                        "    --no-trailn,      -n\t\t\tDon't print trailing newline at the end of output (not the newline a file may have).\n"
+                        "    --help,           -h\t\t\tPrint this help message.\n\n"
+                        "Note: -s and -r switches and their respective long options are to be used together.\n");
         exit(status);
     }
 
-    fprintf(stdout, "USAGE: naft OPTION(S) FILE \n"
-                    "  or   naft OPTION(S) -i\n\n");
+    fprintf(stdout, "USAGE: naft OPTION(S)\n"
+                    "  or   naft OPTION(S) -f FILE \n\n");
 
-    fprintf(stdout, "--starting-line,  -s <number>\t\tWhen specifying range (stating). Used with -r switch.\n"
-                    "--required-lines, -r <number>\t\tWhen specifying range (ending). Used with -s switch.\n"
-                    "--all-after,      -a <number>\t\tPrint all lines after given line.\n"
-                    "--just-one,       -o <number>\t\tPrint one line after the given line.\n"
-                    "--from-stdin,     -i\t\t\tGet input from stdin instead of from FILE.\n"
-                    "--help, -h\t\t\t\tPrint this help message.");
+    fprintf(stdout, "    --starting-line,  -s <number>\t\tSpecify range (stating). Used with -r switch.\n"
+                    "    --ending-line,    -r <number>\t\tSpecify range (ending). Used with -s switch.\n"
+                    "    --all-after,      -a <number>\t\tPrint all lines after given line.\n"
+                    "    --just-one,       -o <number>\t\tPrint one line after the given line.\n"
+                    "    --from-file,      -f\t\t\tGet input from stdin instead of from stdin.\n"
+                    "    --no-trailn,      -n\t\t\tDon't print trailing newline at the end of output (not the newline a file may have).\n"
+                    "    --help,           -h\t\t\tPrint this help message.\n\n"
+                    "Note: -s and -r switches and their respective long options are to be used together.\n");
     exit(status);
 }
